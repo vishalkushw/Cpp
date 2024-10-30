5a05d266bdf35ef4b7a88723f52cc8c40d11bd1a
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-10-31 00:14:56 +0530

    Class code cpp

---
+++ Class code C++/Aug16 question.cpp
@@ -0,0 +1,16 @@
+// 01 Take positive integer input and tell if it is even or odd
+
+// 02 Take positive integer input and tell if it is divisible by 5 or not
+
+/* 03 If cost price and selling price of an item is input through the 
+    keyboard, write a program to determine whether the seller has 
+    made profit or incurred loss or no profit no loss.
+    Also determine how much profit he made or loss he incurred.*/
+//04 Take positive integer input and tell if it is divisible by 5 and 3
+
+// 05 Take positive integer input and tell if it is divisible by 5 or 3
+// but not divisible by 15
+
+//06
+
+
