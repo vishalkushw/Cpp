c6d65900965445e75f94bf7244f83d9a249bd531
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-03 00:04:15 +0530

    Class code cpp

---
+++ Class code C++/Code chef.cpp
@@ -0,0 +1,66 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//    cout << "Hello " << "World" << 5 << " endl" <<2;
+//}
+
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	char ch='cha';
+//	long num= 95420076541L;
+//	cout<<num;
+//    //cout << "Hello " << endl << " World!";
+//}
+
+//Declare two integer variables a and b.
+//Assign the value 20 to a and 6 to b.
+//print the result of a / b.
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a=20;
+//	int b=6;
+//	cout<<a/b;
+//}
+
+//How to accept integer inputs 
+//Let us define a variable ? N that accepts integer inputs.
+//Output the same variable ?N.To declare an ? integer variable, we use the ?
+//int data type.
+
+//#include <bits/stdc++.h>
+//using namespace std;
+//int main()
+//{
+//    int N;
+//    cin >> N;
+//    cout<< N;
+//}
+//
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    int A, B, C, D, E;
+//    cin>>A;
+//    cin>>B;
+//    cin>>C;
+//    cin>>D;
+//    cin>>E;
+//    cout << A << endl;
+//    cout << B << endl;
+//    cout << C << endl;
+//    cout << D << endl;
+//    cout << E << endl;
+//}
+
+
+
+
+
+
