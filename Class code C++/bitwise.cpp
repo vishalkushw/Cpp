8414996186a9072087fa27e159f34392bae7763d
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-02 16:12:40 +0530

    Class code cpp

---
+++ Class code C++/bitwise.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int a=6;
+	int b=2;
+	cout<<(a<<b);  //rigth sift oprater double by number
+	cout<<(a>>2);  // left shift oprater half by number
+}
---
+++ Class code C++/biwise.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int a=50;
+	int b=3;
+	cout<<(a<<b); //50<<3   100  200 400
+	
+}
