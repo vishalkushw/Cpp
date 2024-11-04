a8f696ca3c10fda1b4a6047718438f7c3da6a8f2
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-04 19:51:14 +0530

    Class note cpp

---
+++ Class code C++/comparsion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	int a = 12;
+	int b = 34;
+	
+       // this is single line comment
+       /* this is multiline comment */
+
+//	cout<<(a<b)<<endl; //a is lessthan b
+	
+	
+//	cout<<(a==b)<<endl; //a is not eqal b
+	
+	cout <<  !(a>=b);
+	
+	
+}
