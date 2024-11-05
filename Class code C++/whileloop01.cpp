208fa034d76751fec6f618e5f152bc5ae3c84861
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-05 11:41:43 +0530

    Class code cpp

---
+++ Class code C++/whileloop01.cpp
@@ -0,0 +1,66 @@
+
+
+/*
+//03
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter number : ";
+	cin>>n;
+	
+   	if(n%7==0){
+	   
+	    for(int i=7;i<=100;i+=7){
+	
+			cout<<i<<" "; 
+        }
+    }
+    
+      else if(n%11==0) {
+      	
+        	for(int i=11;i<100; i+=11){
+		  
+	       cout<<i<<" ";
+	}
+	       }
+	       
+	       else{
+	       	cout<<"no match";
+		   }
+
+}
+*/
+/*
+//04
+#include <iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"enter number : ";
+	cin>>n;
+	
+   	if(n%7==0){
+	   int i=7;
+	    while(i<=100){
+	
+			cout<<i<<" "; 
+			i+=7;
+        }
+    }
+    
+      else if(n%11==0) {
+      	int i=11;
+        	while(i<100 ){
+		  
+	       cout<<i<<" ";
+	       i+=11;
+	}
+	       }
+	       
+	       else{
+	       	cout<<"no match";
+		   }
+
+}
+*/
