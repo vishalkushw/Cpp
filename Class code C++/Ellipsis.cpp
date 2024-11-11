a9659169ab096ab6e6e2c57c41c25649ebf7eb03
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-11 11:35:26 +0530

    Class code cpp

---
+++ Class code C++/Ellipsis.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    char choice = 'y';
+
+    while (choice == 'y') {
+        try {
+            cout << "Enter two numbers: ";
+            cin >> num1 >> num2;
+
+            if (num1 + num2 > 100) {
+                throw num1+num2;
+            }
+
+            cout << "Sum: " << num1 + num2 << endl;
+
+            cout << "Do you want to continue  'y' or 'e' : ";
+            cin >> choice;
+
+            if (choice != 'y' && choice != 'e') {
+                throw choice;
+            }
+        }
+    
+        catch (...) {
+            cout << "EXCEPTION: ";
+        }
+    }
+}
+
