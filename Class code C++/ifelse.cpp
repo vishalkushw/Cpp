e15a216ebd9162ba6c4ed756e84b3e626b6337e5
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-17 00:05:02 +0530

    class code cpp

---
+++ Class code C++/ifelse.cpp
@@ -0,0 +1,28 @@
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int year;
+	double salary,bonus;
+	string name;
+	cout<<"enter your name : ";
+	cin>>name;
+	
+	cout<<"enter year of service and current salary : ";
+	cin>>year>>salary;
+	
+	cout<<"Hellow " <<name<<" your salary detail " <<endl;  // \n
+	if(year>5){
+		bonus=salary*(5.0/100);
+		cout<<"total bonus"<<bonus<<endl;
+		cout<<"salary : "<<salary+bonus<<endl;
+	}
+	else{
+		cout<<"total bonus:"<<0<<endl;
+		cout<<"salary"<<salary<<endl;
+	}
+}
+
+*/
+
