6c39c5d5dbbcf5be9c05a72baaeb704f26721ce0
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-20 14:05:23 +0530

    class code cpp

---
+++ Class code C++/july02.cpp
@@ -0,0 +1,67 @@
+// write a program to  finde the number of student to head failed in a examination
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	int marks[8]={37,44,5,67,87,36,23,39};
+	int n=8;
+	for (int i=0;i<n;i++){
+		cout<<marks[i]<<" ";
+	}
+	cout<<endl;
+	for(int i=0;i<n;i++){
+		if(marks[i]<40){
+			cout<<"failed student index "<<i<<" :"<<marks[i]<<endl;
+		}
+	}
+	
+	for(int i=0;i<n; i++){
+		cout<<marks[i]<<" ";
+	}
+}
+
+//2
+#include <iostream>
+using namespace std;
+int main(){
+	int marks[8]={37,44,5,67,87,36,23,39};
+	int n=8;
+	for (int i=0;i<n;i++){
+		cout<<marks[i]<<" ";
+	}
+	cout<<endl;
+	
+	for(int i=0;i<n;i++){
+		if(marks[i]>=35 && marks[i]<40){
+			cout<<"grace given to index "<<i<<" student : "<<40-marks[i]<<endl;
+			marks[i]= 40;
+		}
+	
+//
+	for(int i=0;i<n; i++){
+		cout<<marks[i]<<" ";
+	}
+}
+}
+*/
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int temp,last;
+//	int marks[8]={37,44,5,67,87,36,23,39};
+//	int n=8;
+//	temp=marks[n];
+//	for (int i=0;i<n;i++){
+//		cout<<marks[i]<<" ";
+//	}
+//	cout<<endl;
+//	for(int i=0;i<n;i++){
+//		
+//		}
+//	}
+//	
+//	for(int i=0;i<n; i++){
+//		cout<<marks[i]<<" ";
+//	}
+//}
+
