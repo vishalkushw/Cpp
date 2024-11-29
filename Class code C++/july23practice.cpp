b10d415e7066cf012b897833c4028c090878e2b8
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-11-29 11:53:17 +0530

    class code cpp

---
+++ Class code C++/july23practice.cpp
@@ -0,0 +1,28 @@
+//variable
+
+//type of variable   global static local
+
+//Keyword
+
+//constant(literals)
+
+//identifier
+
+//operators    
+    /* Arithmetic Relational Logical Assignment Ternary Bitwise */
+
+//conditional statement
+   /* if  if-else nested if else  else-if  switch */
+ 
+//Looping Statement 
+   /* while   do-while   for   nested loop*/
+
+//jumping Statement
+   //break   continue   goto   exit   return 
+ 
+//Derived datatype
+   /* array  string  pointer  function  */
+      
+
+
+
