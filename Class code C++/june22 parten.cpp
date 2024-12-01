17f216d08b3746af6499c37651a23cb9661eaeda
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-02 01:00:37 +0530

    cpp class code

---
+++ Class code C++/june22 parten.cpp
@@ -0,0 +1,90 @@
+/*
+#include <iostream>
+using namespace std;
+int main(){
+      
+      for(int i=1; i<=5; i++){
+      	for(int j=1;j<=i;j++){
+      		cout<<"*";
+		  }
+		 cout<< endl;
+	  }
+}
+
+
+//02
+#include <iostream>
+using namespace std;
+int main(){
+      
+      for(int i=5; i>=1; i--){
+      	for(int j=1;j<=i;j++){
+      		cout<<"*";
+		  }
+		 cout<< endl;
+	  }
+}
+
+
+//
+#include <iostream>
+using namespace std;
+int main(){
+      
+      for(int i=1; i<=4; i++){
+      	for(int j=1;j<=i;j++){
+      		cout<<" * ";
+		  }
+		 cout<< endl;
+	  }
+	  
+	  for(int i=5; i>=1; i--){
+      	for(int j=1;j<=i;j++){
+      		cout<<" * ";
+		  }
+		 cout<< endl;
+	  }
+}
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+      
+      for(int i=1; i<=5; i++){
+      	for(int j=1;j<=5;j++){
+      		if(i==1||j==1||i==5||j==5){
+			  
+      		cout<<"*";
+      		
+			   }
+			else{
+			
+		    cout<< " ";
+		}
+	  }
+	  cout<<endl;
+}
+}
+*/
+
+#include <iostream>
+using namespace std;
+int main(){
+      
+      for(int i=1; i<=5; i++){
+      	for(int j=1;j<=5;j++){
+      		if(i==1||j==1||i==5||j==5||i==3||j==3){
+			  
+      		cout<<"*";
+      		
+			   }
+			else{
+			
+		    cout<< " ";
+		}
+	  }
+	  cout<<endl;
+}
+}
