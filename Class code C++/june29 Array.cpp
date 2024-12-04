e03a7812cfe9972e735b5d51ce6c78494a172713
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-04 15:17:28 +0530

    class code cpp

---
+++ Class code C++/june29 Array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main(){
+//	int Arr[5]={23,45,'A',21,26};
+//
+//	cout<<Arr[2];
+
+
+//int arr[8];  //declaration
+//arr[0]=56;
+//arr[1]=45;
+//arr[7]=23;
+//cout<<arr[7];
+
+
+//
+int arr[5]={34,56,76.65,'A',87};   //data lose index    Not -->  in latest compiler can typecast data  as      
+cout<<arr[3];                                           // double-->int-->char
+                                                          //8          4     1    it is posiblae       but bigger to small not posile   int to double  error throw
+//double arr[5]={34,56,76.65,'B',87};   //data lose index  
+//cout<<arr[3];
+
+}
+
+
+
