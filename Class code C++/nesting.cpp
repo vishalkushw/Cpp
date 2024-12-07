15b5180e7817fbb90329e6cbeb857aec8c058b1c
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-07 19:31:30 +0530

    Add files via upload

---
+++ Class code C++/nesting.cpp
@@ -0,0 +1,31 @@
+// 01 Nesting : a complex statement inside the body of another complex statement               19/06/24
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	
+	for(int i=1;i<=3;i++) {   //outer loop        simple 
+		cout<<i<<"set \n";
+		for(int j=1;j<=10;j++){     //inner loop
+			cout<<j<<"repettion \n";
+		}
+		
+}
+}
+*/
+
+// 02 
+#include <iostream>
+using namespace std;
+int main(){
+	
+	for(int i=1;i<=3;i++) {   //outer loop        simple 
+		cout<<i<<"set \n";
+		for(int j=1;j<=10;j++){     //inner loop
+			cout<<j<<"repettion \n";
+		}
+		
+}
+}
+
+
