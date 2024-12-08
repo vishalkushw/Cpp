5dfa5ac57af8fa91b5325a3a01a4e2fea2ce0ed3
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-08 15:42:09 +0530

    class code cpp

---
+++ Class code C++/oddeven.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int number;
+	cout<<"Enter a number : ";
+	cin>>number;
+	if(number%2==0){
+		cout<<"even number:";	
+	}
+    else{
+	    cout<<"odd number";
+  }
+}
