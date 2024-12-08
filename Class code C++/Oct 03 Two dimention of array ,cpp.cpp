178988c2576daeccee1f30ee1072b005ac3841f5
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-08 15:41:16 +0530

    class code cpp

---
+++ Class code C++/Oct 03 Two dimention of array ,cpp.cpp
@@ -0,0 +1,10 @@
+                //Two dimention of array 
+ /*  In a two dimention arry we use two subscript where  first subscript 
+     belong two row and second the subscript belong two columb mostaly
+    database uses table as two dimention array in a two dimention 
+     array we use two nested loop where first loop dedicated for row and
+     the second loop is dedicated for columb it is also called matrics
+     
+     syntax datatype arrayname [rowsize] [columbsize];
+ */    
+ 
