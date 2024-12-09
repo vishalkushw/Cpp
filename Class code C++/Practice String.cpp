fe5755214cad66425d26dd622854496b87d81652
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-09 21:03:56 +0530

    class code cpp

---
+++ Class code C++/Practice String.cpp
@@ -0,0 +1,95 @@
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	string str="Vishal kushwah";
+//	cout<<str<<endl;
+//	int size=str.length();
+//	
+//	for(int i=size;i>=0;i--){
+//		cout<<str[i];
+//	}
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	char str[6]={'v','i','s','h','a','l'};
+//	cout<<str<<endl;
+//	
+//	for(int i=6;i>=0;i--){
+//		cout<<str[i];
+//	}
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	string str="Vishal ";
+//	string str1="kushwah";
+//	cout<<str.append(str1)<<endl;	
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	string str;
+//	cout<<"enter your string name:";
+//	getline(cin,str);
+//	cout<<str;   	
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	string str="c++ is a power fulllanguage";
+//	int count=0;
+//	for(int i=0;i<str.size();i++){
+//		if(str[i]==' '){
+//			count++;
+//		}
+//	}
+//	cout<<count;
+//}
+
+//
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	string str="c++ is a power fulllanguage";
+//	for(int i=0;i<str.size();i++){
+//		if(str[i]==' '){
+//			continue;
+//		}
+//	cout<<str[i];
+//	}
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	string str = "apple";
+//	int index= str.find('a');
+//	cout<<index;
+//}
+
+
+// space remove in string 
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	string name= "cpp is a powerful language";
+//	string name1;
+//	int size=name.size();
+//	
+//	for(int i=0;i<size;i++){
+//		if(name[i]==' '){
+//			continue;
+//		}
+//		name1=name1+name[i];
+//	}
+//	cout<<name1;
+//}
+
+
