61085083c90a9e7da0a67979a9f01a639d683b57
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-09 21:03:08 +0530

    class code cpp

---
+++ Class code C++/Practice array.cpp
@@ -0,0 +1,152 @@
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"enter size of arr:";
+//	cin>>n;
+//	int arr[n];
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"enter arr eliment of index "<<i<<" ";
+//		cin>>arr[i];
+//	}
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"arr eliment of index "<<i<<" "<<arr[i]<<endl;
+//	
+//	}
+//
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"enter size of arr:";
+//	cin>>n;
+//	int arr[n];
+//	int sum=0;
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"enter arr eliment of index "<<i<<" ";
+//		cin>>arr[i];
+//	}
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"arr eliment of index "<<i<<" "<<arr[i]<<endl;
+//		if(arr[i]%2==0){
+//			sum=sum+arr[i];
+//		}
+//	
+//	}
+//  cout<<sum;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"enter size of arr:";
+//	cin>>n;
+//	int arr[n];
+//	int sum=0;
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"enter arr eliment of index "<<i<<" ";
+//		cin>>arr[i];
+//	}
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"arr eliment of index "<<i<<" "<<arr[i]<<endl;
+//		if(arr[i]%2==1){
+//			sum=sum+arr[i];
+//		}
+//	
+//	}
+//  cout<<"odd sum is :"sum;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"enter size of arr:";
+//	cin>>n;
+//	int arr[n];
+//	int sum=0;
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"enter arr eliment of index "<<i<<" ";
+//		cin>>arr[i];
+//	}
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"arr eliment of index "<<i<<" "<<arr[i]<<endl;
+//			sum=sum+arr[i];
+//	}
+//  cout<<"odd sum is :"<<sum;
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int n;
+//	cout<<"enter size of arr:";
+//	cin>>n;
+//	int arr[n];
+//	int sum=1;
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"enter arr eliment of index "<<i<<" ";
+//		cin>>arr[i];
+//	}
+//	
+//	for(int i=0;i<n;i++){
+//		cout<<"arr eliment of index "<<i<<" "<<arr[i]<<endl;
+//			sum=sum*arr[i];
+//	}
+//  cout<<"product is :"<<sum;
+//}
+//
+//#include <iostream>
+//using namespace std;
+//int main(){
+//
+//	int arr[5]={23,45,22,78,34};
+//	int low=arr[0];
+//	
+//	for(int i=0;i<5;i++){
+//			if(arr[i]<low){
+//				low=arr[i];
+//			}
+//	}
+//  cout<<"low is :"<<low;
+//}
+
+////write a priogram finde the higest and second highest number in an arry
+//
+//#include <iostream>
+//using namespace std;
+//int main(){
+//
+//	int A[6]={45,66,29,56,322,11};
+//    int high,shigh;
+//    if(A[0]>A[1]){
+//    	high=A[0];
+//    	shigh=A[1];
+//	}
+//	else{
+//		high=A[1];
+//    	shigh=A[0];
+//	}
+//	
+//	for (int i=2;i<6;i++){
+//		if(A[i]>high){
+//			shigh=high;
+//			high=A[i];	
+//		}	
+//	}
+//	cout<<"higest : "<<high<<endl;
+//	cout<<"shigh : "<<shigh;
+//}
+
