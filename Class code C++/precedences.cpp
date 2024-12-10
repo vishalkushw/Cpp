4b5b9ca6b0faaea97f7aec0bf652b69ca21a4329
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-10 08:05:19 +0530

    class code cpp

---
+++ Class code C++/precedences.cpp
@@ -0,0 +1,88 @@
+/*1
+#include <iostream>
+using namespace std;
+int main(){
+	int a=12,b=10,c=1,d=4,e=2;
+	int x = a-b*e+c/d*a*b;
+	cout<<x;
+}
+//-8
+*/
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a=12,b=10,c=1,d=4,e=2;
+//	int x = a>b>c>d>e;
+//	cout<<x;
+//}
+
+//0
+
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	int a=12,b=10,c=1,d=4,e=2;
+	int x = a<b>c<d>e<2;
+	cout<<x;
+}
+// 1
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a=12,b=10,c=1,d=4,e=2;
+	int x = a&b/6*4||d*4>>e;
+	cout<<x;
+}
+
+//1
+
+
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a=12,b=10,c=1,d=4,e=2;
+	int x = a-b*e+c/d*a*b;
+	cout<<x;
+}
+//-8
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a=12,b=10,c=1,d=4,e=2;
+	int x = a*b-c/d+e;
+	cout<<x;
+	
+}
+//122
+
+
+#include <iostream>
+using namespace std;
+int main(){
+	int a=12,b=10,c=1,d=4,e=2;
+	int x = a*(b-c)/d+e;
+	cout<<x;
+}
+//29
+*/
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int a=12,b=10,c=1,d=4,e=2;
+//	int x = a>b>c;
+//	cout<<x;
+//}
+//
+//
+
+
+
+
+
