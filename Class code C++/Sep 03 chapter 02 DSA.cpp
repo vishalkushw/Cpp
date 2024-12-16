e516e2ca5c9fa3081ce7abf500b71d9b20d8c23b
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-16 15:20:12 +0530

    class code cpp

---
+++ Class code C++/Sep 03 chapter 02 DSA.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main(){
+//	cout<<"Hii good mrning \nVishal ";
+//	// variable
+//	int age=20;
+//	cout<<age<<endl;
+//	cout<<"size of int : "<<sizeof(age);
+//	
+
+  // cin
+//  int a;
+//  cout<<"enter your age:";
+//  cin>>a;
+//  cout<<"Age:"<<a;
+
+// oprater
+
+int a,b;
+  cout<<"enter your num1:";
+  cin>>a;
+  cout<<"enter your num2:";
+  cin>>b;
+  
+  cout<<"sum:"<<a+b;
+	
+}
