c0fd247a76fea57b425b09d8cd56ec53e0bb745c
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2024-12-19 14:21:37 +0530

    class code cpp

---
+++ Class code C++/Sep 26 array.cpp
@@ -0,0 +1,31 @@
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	char s[]="welcome";
+//	for(int i=0;s[i]!='\0';i++){ //use only character  no use in int 
+//		cout<<s[i]; 
+//	}
+//}
+
+//#include <iostream>
+//using namespace std;
+//int main(){
+//	int arr[]={10,20,30 ,40,78};
+////	cout<<*arr<<endl;  //10//addres value of 0 index
+////	cout<<*arr+1<<endl;  //11
+////	cout<<*(arr+1)<<endl;//20
+//	cout<<2[arr]<<endl;
+//	cout<<-2[arr]<<endl;
+//	cout<<arr[-2]; //garbage value   draw back in array 
+//}
+
+#include <iostream>
+using namespace std;
+int main(){
+	int arr[5]={10,20,30 ,40,78};
+
+	cout<<arr[1];
+}
+
+
+
