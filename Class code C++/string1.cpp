340a0f8b6517d6bf6918d6925841940788645995
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-01-03 10:33:22 +0530

    class code cpp

---
+++ Class code C++/string1.cpp
@@ -0,0 +1,26 @@
+
+/*
+#include <iostream>
+using namespace std;
+int main(){
+	string f1= "Apple is good";
+
+        int size=f1.size();
+      
+      for( int i=0; i<size;i++){
+
+      	if(f1[i]=='p'||f1 [i]=='o'){
+      		f1[i]='$';
+		  }
+		  
+	  }
+      
+    cout<<f1;
+
+	}
+	
+		
+*/	
+
+
+
