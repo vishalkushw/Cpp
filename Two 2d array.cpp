5666160820f75c88750ebccc330de436d8815b54
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-01-07 12:21:52 +0530

    class code cpp

---
+++ Two 2d array.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){ 
+    int arr1[2][3]={1,2,5,6,7,5};
+    
+    for(int i=0;i<2;i++){
+        for(int j=0;j<3;j++){
+         cout<<arr1[i][j]<<" ";
+        }
+    }
+
+}
