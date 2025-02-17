0982525ea96ef6117c664dbb069f6fb5a1768ad8
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-02-18 00:08:10 +0530

    Update Array print.cpp

--- Class code C++/Array print.cpp
+++ Class code C++/Array print.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std ;
 int main(){
-	int arr[5]={1,2,3,4,5};
+	int arr[6]={1,2,3,4,5,6};
 	cout<<"Array : ";
-	for(int i=0;i<5;i++){
+	for(int i=0;i<6;i++){
 		cout<<arr[i]<<" ";
 	}
 }
