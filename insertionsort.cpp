6bf6d2ba04c589af52ac8c39a285d8f900245bb5
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-05-26 21:58:44 +0530

    insertionsort

---
+++ insertionsort.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void insrt(int arr[],int n){
+    for(int i=1;i<n;i++){
+        int key=i;
+        while (key>0 && arr[key-1]>arr[key])
+        {
+            int temp=arr[key-1];
+            arr[key-1]=arr[key];
+            arr[key]=temp;
+            key--;
+        }
+    }
+}
+int main(){
+int arr[]={3,1,4,2,6};
+int s=sizeof(arr)/sizeof(arr[0]);
+cout<<"Before sorting"<<endl;
+for(int i=0;i<s;i++){
+cout<<arr[i]<<" ";
+}
+insrt(arr,s);
+cout<<"\nAfter sorting"<<endl;
+for(int i=0;i<s;i++){
+cout<<arr[i]<<" ";
+}
+}
