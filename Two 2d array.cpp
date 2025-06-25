f71a24515f0975b8f3d490e06135a5c2f28ac92d
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-06-25 22:00:19 +0530

    Update Two 2d array.cpp

--- Two 2d array.cpp
+++ Two 2d array.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){ 
-    int arr1[2][3]={1,2,5,6,7,5};
+    int arr1[2][3]={1,2,8,6,7,5};
     
     for(int i=0;i<2;i++){
         for(int j=0;j<3;j++){
