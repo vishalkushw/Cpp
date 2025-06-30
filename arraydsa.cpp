7b71dc69121c65b965a543aca8a8969817c84e3e
Vishal kushwah <110225964+vishalkushw@users.noreply.github.com>
2025-06-30 16:41:15 +0530

    Update arraydsa.cpp

--- arraydsa.cpp
+++ arraydsa.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #include <array>
 int main(){
 
-    array<int ,6>b{11,2,3,1,5,6};
+    array<int ,6>b{11,2,4,1,5,6};
 //     cout<<"size="<<b.size()<<" \n";
 //     for(int i=0;i<b.size();i++){
 //         cout<<b[i]<<"\t";
